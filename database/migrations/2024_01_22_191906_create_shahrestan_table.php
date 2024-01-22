48147aff80a0784b6bba7bb398a01b042247d5fd
mohammadreza <mrzhatam@gmail.com>
2024-01-22 11:25:23 -0800

    Update 2024_01_22_191906_create_shahrestan_table.php

--- database/migrations/2024_01_22_191906_create_shahrestan_table.php
+++ database/migrations/2024_01_22_191906_create_shahrestan_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('shahrestan', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->foreignId('ostan');
             $table->foreign('ostan')->references('id')->on('ostan');
         });
     }
