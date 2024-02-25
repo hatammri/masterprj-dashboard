6690023b86d7515735f0af1a0381c8af96aeef57
mohammadreza <mrzhatam@gmail.com>
2024-02-25 08:33:15 -0800

    update equipment

--- app/Http/Controllers/Admin/EquipmentController.php
+++ app/Http/Controllers/Admin/EquipmentController.php
@@ -28,14 +28,15 @@ class EquipmentController extends Controller
     {
         $brand = Brand::all();
         $typeEquipment = TypeEquipment::all();
-        return view('equipment.create', compact('brand','typeEquipment'));
+        return view('equipment.create', compact('brand', 'typeEquipment'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
-    {   dd($request);
+    {
+        dd($request);
         $request->validate([
             'name' => 'required',
             'price' => 'required',
@@ -58,7 +59,7 @@ class EquipmentController extends Controller
             'type_equipment_id.required' =>  'نوع تجهیز مشخص نشده',
 
         ]);
-       try {
+        try {
             Equipment::create([
                 'name' => $request->name,
                 'price' => $request->price,
@@ -72,7 +73,7 @@ class EquipmentController extends Controller
             ]);
             Alert::success('تجهیز ایجاد گردید', 'باتشکر');
             return redirect()->route('equipment.index');
-       } catch (\Illuminate\Database\QueryException $e) {
+        } catch (\Illuminate\Database\QueryException $e) {
             Alert::error('اطلاعات تجهیز تکراری و یا اشتباه است', 'خطا');
             return back();
             dd($e->getMessage(), $e->errorInfo);
@@ -106,34 +107,41 @@ class EquipmentController extends Controller
      * Update the specified resource in storage.
      */
     public function update(Request $request, Equipment $equipment)
-    { //dd($request,$equipment);
-
+    {  //dd($request,$equipment);
         $request->validate([
             'name' => 'required',
-            'email' => 'required',
-            'phonenumber' => 'required',
+            'price' => 'required',
+            'color' => 'required',
+            'equipment_security' => 'required',
+            'weight' => 'required',
+            'dimensions' => 'required',
             'description' => 'required',
-            'company' => 'required',
-            'rule' => 'required'
+            'brand_id' => 'required',
+            'type_equipment_id' => 'required'
         ], $messages = [
-            'name.required' => 'نام مشتری نباید خالی باشد',
-            'email.required' => 'ایمیل نباید خالی باشد',
-            'phonenumber.required' => 'شماره همراه نباید خالی باشد',
-            'description.required' => 'توضیحات نباید خالی باشد',
-            'company.required' =>  'شرکت نباید خالی باشد',
-            'rule.required' =>  'نقش نباید خالی باشد',
+            'name.required' => 'نام تجهیز نباید خالی باشد',
+            'price.required' => 'قیمت تجهیز نباید خالی باشد',
+            'color.required' => 'رنگ تجهیز نباید خالی باشد',
+            'equipment_security.required' => 'امنیت تجهیز را مشخص کنید',
+            'weight.required' =>  'وزن نباید خالی باشد',
+            'dimensions.required' =>  'ابعاد نباید خالی باشد',
+            'description.required' =>  'توضیحات نباید خالی باشد',
+            'brand_id.required' =>  'برند تجهیز مشخص نشده',
+            'type_equipment_id.required' =>  'نوع تجهیز مشخص نشده'
         ]);
-
         try {
             $equipment->update([
                 'name' => $request->name,
-                'email' => $request->email,
-                'phonenumber' => $request->phonenumber,
+                'price' => $request->price,
+                'color' => $request->color,
+                'equipment_security' => $request->equipment_security,
+                'weight' => $request->weight,
+                'dimensions' => $request->dimensions,
                 'description' => $request->description,
-                'company' =>  $request->company,
-                'rule' => $request->rule,
+                'brand_id' => $request->brand_id,
+                'type_equipment_id' => $request->type_equipment_id
             ]);
-            Alert::success('مشتری مورد نظر ویرایش شد', 'باتشکر');
+            Alert::success('تجهیز مورد نظر ویرایش شد', 'باتشکر');
             return redirect()->route('equipment.index');
         } catch (\Illuminate\Database\QueryException $e) {
             //     // You need to handle the error here.     //     // Either send the user back to the screen or redirect them somewhere else
@@ -165,6 +173,4 @@ class EquipmentController extends Controller
             JSON_UNESCAPED_UNICODE
         );
     }
-
-
 }
