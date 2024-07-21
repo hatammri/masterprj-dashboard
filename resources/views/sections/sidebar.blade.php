9fe786d1ce157fc386923ed5b14af2210d9519ed
hatammri <mrzhatam@gmail.com>
2024-07-21 08:19:49 +0330

    Update sidebar.blade.php

--- resources/views/sections/sidebar.blade.php
+++ resources/views/sections/sidebar.blade.php
@@ -378,7 +378,7 @@
         @endcan
 
         {{-- customer  --}}
-        {{-- @canany(['Customer_Create_Admin', 'Customer_List_Admin']) --}}
+        @canany(['Customer_Create_Admin', 'Customer_List_Admin'])
             <li class="menu-item">
                 <a href="#" class="menu-link menu-toggle">
                     <i class="menu-icon tf-icons bx bx-group"></i>
@@ -386,54 +386,54 @@
                 </a>
 
                 <ul class="menu-sub">
-                    {{-- @can('Customer_List_Admin') --}}
+                    @can('Customer_List_Admin')
 
                         <li class="menu-item">
                             <a href="{{ route('customer.index') }}" class="menu-link">
                                 <div>لیست مشتری</div>
                             </a>
                         </li>
-                    {{-- @endcan --}}
+                    @endcan
 
-                    {{-- @can('Customer_Create_Admin') --}}
+                    @can('Customer_Create_Admin')
 
                         <li class="menu-item">
                             <a href="{{ route('customer.create') }}" class="menu-link">
                                 <div>اضافه کردن مشتری</div>
                             </a>
                         </li>
-                    {{-- @endcan --}}
+                    @endcan
 
                 </ul>
             </li>
-        {{-- @endcan --}}
+        @endcan
         {{-- permission  --}}
-        {{-- @canany(['Permission_Create_Admin', 'Permission_List_Admin']) --}}
+        @canany(['Permission_Create_Admin', 'Permission_List_Admin'])
             <li class="menu-item">
                 <a href="#" class="menu-link menu-toggle">
                     <i class="menu-icon tf-icons bx bx-universal-access"></i>
                     <div>سطوح دسترسی</div>
                 </a>
                 <ul class="menu-sub">
-                    {{-- @can('Permission_List_Admin') --}}
+                    @can('Permission_List_Admin')
 
                     <li class="menu-item">
                         <a href="{{ route('permission.index') }}" class="menu-link">
                             <div> لیست Permissions</div>
                         </a>
                     </li>
-                    {{-- @endcan --}}
-                    {{-- @can('Permission_Create_Admin') --}}
+                    @endcan
+                    @can('Permission_Create_Admin')
 
                     <li class="menu-item">
                         <a href="{{ route('permission.create') }}" class="menu-link">
                             <div>اضافه کردن Permissions </div>
                         </a>
                     </li>
-                    {{-- @endcan --}}
+                    @endcan
                 </ul>
             </li>
-            {{-- @endcan --}}
+            @endcan
 
             {{-- <li class="menu-item">
             <a href="#" class="menu-link">
