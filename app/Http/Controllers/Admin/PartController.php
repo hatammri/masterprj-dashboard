21af39b2cf76b2390abebb3ee4c3f71e7a4f5a21
hatammri <mrzhatam@gmail.com>
2024-08-05 23:04:10 +0330

    Update PartController.php

--- app/Http/Controllers/Admin/PartController.php
+++ app/Http/Controllers/Admin/PartController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\part;
+use App\Models\Part;
 use App\Models\Ostan;
 use App\Models\Shahrestan;
 use RealRashid\SweetAlert\Facades\Alert;
